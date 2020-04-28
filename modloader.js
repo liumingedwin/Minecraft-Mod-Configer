52535cd5fc81cad33033d6829d7a056441ce45d3
liumingedwin <liumingedwin@163.com>
2020-04-28 11:12:12 +0800

    Add files via upload

--- modloader.js
+++ modloader.js
@@ -1,114 +1,67 @@
-var ws = require("nodejs-websocket");
-var child_process = require('child_process');
-var fs = require("fs")
-console.log("Started to Create...")
-Date.prototype.format = function(fmt)
-{ 
-var o = {
-"M+" : this.getMonth()+1, //月份
-"d+" : this.getDate(), //日
-"h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时
-"H+" : this.getHours(), //小时
-"m+" : this.getMinutes(), //分
-"s+" : this.getSeconds(), //秒
-"q+" : Math.floor((this.getMonth()+3)/3), //季度
-"S" : this.getMilliseconds() //毫秒
-};
-if(/(y+)/.test(fmt))
-fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
-for(var k in o)
-if(new RegExp("("+ k +")").test(fmt))
-fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
-return fmt;
-}
-var server = ws.createServer(function(conn){
-conn.on("text", function (str) {
-	try{
-	var parsed=JSON.parse(str);
-	console.log(parsed);
-	var message=parsed.body.properties.Message,sender=parsed.body.properties.Sender;
-	var argv=message.split(" ");
-	if(argv[0]=="#mod")
-	{
-		if(argv.length<3)
-		conn.sendText(`{
-	"body": {
-		"origin": {
-			"type": "player"
-		},
-		"commandLine": "/tell `+sender+` 参数数量不够",
-		"version": 1
-	},
-	"header": {
-		"requestId": "00000000-0000-0000-0000-000000000000",
-		"messagePurpose": "commandRequest",
-		"version": 1,
-		"messageType": "commandRequest"
+var mcws=require("mcws");
+var running=0;
+var arr=[];
+var server=new mcws(function(choise,message,conn){
+	if(choise=="create"){
+		conn.sendcommand('/say Welcome Use Mod-loader which is develop by edwinlao');
+		conn.sendcommand('/say type ./help to get some information.');
 	}
-}`);
-		if(argv[1]=="add"||argv[1]=="apply"||argv[1]=="load")
-		{
-		eval(argv[2]+"=require('"+argv[2]+"');");
+	else{
+		if(message.body&&message.body.eventName&&message.body.eventName=="PlayerMessage"){
+		var lmessage=message.body.properties.Message;
+		if(lmessage=="./help"){
+			conn.sendcommand("/say ./help:Show This information.");
+			conn.sendcommand("/say ./addmod %modname%:load a mod");
+			conn.sendcommand("/say ./runmod:Start Mod");
+			conn.sendcommand("/say ./stopmod:Stop mod");
+			conn.sendcommand("/say ./bye:close the websocket");
+			conn.sendcommand("/say ./run %commandname%:Run a command");
 		}
-		if(argv[1]=="del"||argv[1]=="delete"||argv[1]=="unload")
-		{
-			eval(argv[2]+"=undefined;");
+		else if(lmessage.slice(0,2+6)=="./addmod"){
+			arr[arr.length]=require(".\\mods\\"+lmessage.slice(9,16384));
+			conn.sendcommand("/say Loading mod:"+lmessage.slice(9,16384));
 		}
-	}
-	else
-	{
-		if(argv[0]){
-		function poststr(execstr){
-	conn.sendText(`{
-	"body": {
-		"origin": {
-			"type": "player"
-		},
-		"commandLine": "`+execstr+`",
-		"version": 1
-	},
-	"header": {
-		"requestId": "00000000-0000-0000-0000-000000000000",
-		"messagePurpose": "commandRequest",
-		"version": 1,
-		"messageType": "commandRequest"
-	}
-}`);
+		else if(lmessage=="./runmod"){
+			console.log("Mod was loaded");
+			running=1;
+			conn.sendcommand("/say Mod was loaded");
+			for (var stc=0;stc<arr.length;stc++)
+				if(arr[stc].onmessage("create",message,conn))return;
 		}
-		var strr=argv[0]+".onmessage(message,argv,poststr,sender);";
-		eval(strr)
-		if(eval(argv[0]).needtime)
-		{
-			setInterval(eval(argv[0]).ontime,eval(argv[0]).timeinterval);
+		else if(lmessage=="./stopmod"){
+			console.log("Mod was stoped");
+			running=0;
+			conn.sendcommand("/say Mod was stoped");
+		}
+		else if(lmessage=="./bye"){
+			console.log("WebSocket was stoped");
+			running=0;
+			conn.sendcommand("/say WebSocket was stoped");
+			conn.sendcommand("/say Thanks for use this addon.");
+			conn.sendcommand("/closewebsocket");
+		}
+		else if(lmessage.slice(0,2+3)=="./run"){
+			conn.sendcommand(lmessage.slice(6,16384));
+		}
+		else{
+				if(running==0){
+				if(lmessage.slice(0,2)=="./"){
+					conn.sendcommand("/say You sended "+lmessage.slice(0,2+3));
+					conn.sendcommand("/say Unknown command because you haven't started the server.Now we'll show some helps.");
+					conn.sendcommand("/say ./help:Show This information.");
+					conn.sendcommand("/say ./modload %modname%:load a mod");
+					conn.sendcommand("/say ./runmod:Start Mod");
+					conn.sendcommand("/say ./stopmod:Stop mod");
+					conn.sendcommand("/say ./bye:close the websocket");
+					conn.sendcommand("/say ./run %commandname%:Run a command");
+				}
+				
+			}
 		}
 		}
+		if(running){
+			for (var stc=0;stc<arr.length;stc++)
+				if(arr[stc].onmessage(choise,message,conn))return;
+		}
 	}
-	}catch(e){
-		console.log(e);
-	}
-})
-    conn.on("close", function (code, reason) {
-        console.log("关闭连接",+code+","+reason)
-});
-    conn.on("error", function (code, reason) {
-        console.log("异常关闭")
-});
-setTimeout(function(){
-		var eventlist=["BossKilled","CraftingSessionCompleted","PlayerTravelled","ScreenChanged","MobKilled","EntitySpawned","PlayerMessage","CommandRequest","CommandResponse"];
-	for (var ss=0;ss<eventlist.length;ss++)
-	        conn.sendText(`{
-	"body": {
-		"eventName": "`+eventlist[ss]+`"
-	},
-	"header": {
-		"requestId": "00000000-0000-0000-0000-000000000000",
-		"messagePurpose": "subscribe",
-		"version": 1,
-		"messageType": "commandRequest"
-	}
-}`);
-},5000);
-
-}).listen(8900)
-
-console.log("WebSocket done")
+},8900);
